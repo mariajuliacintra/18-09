aad40f654a68686808f6aa10a53268d8ca3cc0a2
mariajuliacintra <mariajuliacintra31@gmail.com>
2024-09-18 12:51:46 -0300

    18-09

--- src/controllers/controllerNumbers.js
+++ src/controllers/controllerNumbers.js
@@ -1,6 +1,10 @@
 module.exports = class controllerNumbers{
     static async checkPar(req, res){
         const {numero} = req.body;
+        
+        if(typeof numero !== "number"){
+            return res.status(400).json({message: "Não é um número!!!"})
+        }
 
         if(numero % 2 == 0){
             return res.status(200).json({message: "Número Par!"});
@@ -9,6 +13,10 @@ module.exports = class controllerNumbers{
     }
     static async checkPrimo(req,res){
         const {numero} = req.body;
+
+        if(typeof numero !== "number"){
+            return res.status(400).json({message: "Não é um número!!!"})
+        }
 
         if(numero <= 1){
             return res.status(200).json({message: "Número não é primo"})
